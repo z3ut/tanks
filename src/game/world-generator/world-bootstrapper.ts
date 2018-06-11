ba27d7fe74eed02eb97f5ae8e6697d6e30fd2e97
z3ut <muraview.andrey@gmail.com>
2018-06-11 22:51:37 +0300

    Separate world bootstrapper logic

--- src/game/world-generator/world-bootstrapper.ts
+++ src/game/world-generator/world-bootstrapper.ts
@@ -12,8 +12,10 @@ import { EventTypes } from '../events/event-types';
 import { WorldJsonDescription } from './world-json-description';
 import { TankLogicComponent } from '../tank/tank-logic-component';
 import { GameStatusLogicComponent } from '../game-status/game-status-logic-component';
+import { Boundaries } from '../core/boundaries';
+import { LogicComponent } from '../core/logic-component';
 
-const worldJson = require('./worlds/world_1.json') as WorldJsonDescription;
+const worldJsonDescription = require('./worlds/world_1.json') as WorldJsonDescription;
 
 const aiTankJson = require('./game-objects/ai-tank.json') as { width: number, height: number, speed: number };
 const userTankJson = require('./game-objects/user-tank.json') as { width: number, height: number, speed: number };
@@ -21,22 +23,37 @@ const phoenixJson = require('./game-objects/phoenix.json') as { width: number, h
 
 export class WorldBootstrapper {
 
+  field: any;
+
   bootstrapWorld(id: number): World {
     const world = new World();
-
-    const field = document.querySelector('[data-field]') as any;
-    field.style.width = (worldJson.field.width + 1) * settings.pxPerCoord + 'px';
-    field.style.height = (worldJson.field.height + 1) * settings.pxPerCoord + 'px';
-
     world.gameObjects = [];
-    world.width = worldJson.field.width;
-    world.height = worldJson.field.height;
+    world.width = worldJsonDescription.field.width;
+    world.height = worldJsonDescription.field.height;
 
+    this.initField(world, worldJsonDescription);
+
+    this.addUserTank(world, worldJsonDescription);
+    this.addSpawners(world, worldJsonDescription);
+    this.addGameStat(world, worldJsonDescription);
+    this.addWalls(world, worldJsonDescription);
+    this.addPhoenix(world, worldJsonDescription);
+
+    return world;
+  }
+
+  private initField(world: World, worldJson: WorldJsonDescription) {
+    this.field = document.querySelector('[data-field]') as any;
+    this.field.style.width = (worldJson.field.width + 1) * settings.pxPerCoord + 'px';
+    this.field.style.height = (worldJson.field.height + 1) * settings.pxPerCoord + 'px';
+  }
+
+  private addUserTank(world: World, worldJson: WorldJsonDescription) {
     const userTankInputComponent = new UserTankInputComponent();
     const userTankBasicPhysicComponent = new BasicPhysicComponent(userTankJson.speed);
     const userTankLogicComponent = new TankLogicComponent(EventTypes.UserTankKilled);
     const userTankGraphicComponent = new ImgGraphicComponent(userTankJson.width,
-      userTankJson.height, settings.pxPerCoord, field, ['unit'],
+      userTankJson.height, settings.pxPerCoord, this.field, ['unit'],
       require('~/assets/user-tank.png'));
 
     const userTank = new GameObject(userTankInputComponent, userTankLogicComponent,
@@ -49,47 +66,14 @@ export class WorldBootstrapper {
     userTank.direction = worldJson.field.startDirection;
 
     world.gameObjects.push(userTank);
+  }
 
-    const gameStatusLogicComponent = new GameStatusLogicComponent(worldJson.game.enemiesToKillForWin);
-    const gameStatus = new GameObject(null, gameStatusLogicComponent, null, null);
-    world.gameObjects.push(gameStatus);
-
-    worldJson.gameObjects.walls.brick.forEach(brickWallJson => {
-      for (let x = brickWallJson.leftX; x <= brickWallJson.rightX; x++) {
-        for (let y = brickWallJson.topY; y <= brickWallJson.bottomY; y++) {
-          const brickWall = new GameObject(null, new BrickWallLogicComponent(), null,
-          new ImgGraphicComponent(1, 1, settings.pxPerCoord, field, ['unit'],
-          require('~/assets/wall-brick.png')));
-          brickWall.x = x;
-          brickWall.y = y;
-          brickWall.width = 1;
-          brickWall.height = 1;
-          world.gameObjects.push(brickWall);
-        }
-      }
-    });
-
-    worldJson.gameObjects.walls.steel.forEach(steelWallJson => {
-      for (let x = steelWallJson.leftX; x <= steelWallJson.rightX; x++) {
-        for (let y = steelWallJson.topY; y <= steelWallJson.bottomY; y++) {
-          const steelWall = new GameObject(null, null, null,
-            new ImgGraphicComponent(1, 1, settings.pxPerCoord, field, ['unit'],
-              require('~/assets/wall-steel.png')));
-          steelWall.x = x;
-          steelWall.y = y;
-          steelWall.width = 1;
-          steelWall.height = 1;
-          world.gameObjects.push(steelWall);
-        }
-      }
-    });
-
-
+  private addSpawners(world: World, worldJson: WorldJsonDescription) {
     const aiTankInputComponent = new AiTankInputComponent();
     const aiTankBasicPhysicComponent = new BasicPhysicComponent(aiTankJson.speed);
     const aiTankLogicComponent = new TankLogicComponent(EventTypes.AiTankKilled);
     const aiTankGraphicComponent = new ImgGraphicComponent(aiTankJson.width, aiTankJson.height,
-      settings.pxPerCoord, field, ['unit'], require('~/assets/ai-tank.png'));
+      settings.pxPerCoord, this.field, ['unit'], require('~/assets/ai-tank.png'));
 
     const aiTank = new GameObject(aiTankInputComponent, aiTankLogicComponent,
       aiTankBasicPhysicComponent, aiTankGraphicComponent);
@@ -103,16 +87,45 @@ export class WorldBootstrapper {
     const spawner = new GameObject(null, spawnerLogicComponent, null, null);
     spawner.isNotPhysical = true;
     world.gameObjects.push(spawner);
+  }
 
+  private addGameStat(world: World, worldJson: WorldJsonDescription) {
+    const gameStatusLogicComponent = new GameStatusLogicComponent(worldJson.game.enemiesToKillForWin);
+    const gameStatus = new GameObject(null, gameStatusLogicComponent, null, null);
+    world.gameObjects.push(gameStatus);
+  }
+
+  private addWalls(world: World, worldJson: WorldJsonDescription) {
+    this.addConcreteWalls(world, worldJson.gameObjects.walls.brick,
+      new BrickWallLogicComponent(), require('~/assets/wall-brick.png'));
+    this.addConcreteWalls(world, worldJson.gameObjects.walls.steel,
+      null, require('~/assets/wall-steel.png'));
+  }
+
+  private addConcreteWalls(world: World, boundaries: Boundaries[], logicComponent: LogicComponent, image: any) {
+    boundaries.forEach(wallJson => {
+      for (let x = wallJson.leftX; x <= wallJson.rightX; x++) {
+        for (let y = wallJson.topY; y <= wallJson.bottomY; y++) {
+          const wall = new GameObject(null, logicComponent ? logicComponent.clone() : null, null,
+            new ImgGraphicComponent(1, 1, settings.pxPerCoord, this.field, ['unit'], image));
+          wall.x = x;
+          wall.y = y;
+          wall.width = 1;
+          wall.height = 1;
+          world.gameObjects.push(wall);
+        }
+      }
+    });
+  }
+
+  private addPhoenix(world: World, worldJson: WorldJsonDescription) {
     const phoenix = new GameObject(null, new PhoenixLogicComponent(), null,
       new ImgGraphicComponent(phoenixJson.width, phoenixJson.height,
-        settings.pxPerCoord, field, ['unit'], require('~/assets/phoenix.png')));
+        settings.pxPerCoord, this.field, ['unit'], require('~/assets/phoenix.png')));
     phoenix.x = worldJson.gameObjects.phoenix.x;
     phoenix.y = worldJson.gameObjects.phoenix.y;
     phoenix.width = phoenixJson.width;
     phoenix.height = phoenixJson.height;
     world.gameObjects.push(phoenix);
-
-    return world;
   }
 }
